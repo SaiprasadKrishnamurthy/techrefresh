bfed2f033e1f4330a85d022018539fa8e7442b7f
saiprasad.krishnamurthy <saiprasad.krishnamurthy@gmail.com>
2015-06-16 17:47:17 +0100

    Nth element of a list kata

--- src/main/scala/listutils/ListUtils.scala
+++ src/main/scala/listutils/ListUtils.scala
@@ -1,9 +1,23 @@
 package listutils
 
+import scala.annotation.tailrec
+
 /**
  * Created by sai on 16/06/2015.
  */
 object ListUtils {
+
+  def nthElement(input: List[Int], n: Int): Int = {
+
+    def compute(list: List[Int], counter: Int): Int = {
+      if (list.isEmpty) throw new IndexOutOfBoundsException
+      else if (counter == n) list.head
+      else compute(list.tail, counter + 1)
+    }
+
+    compute(input, 0)
+  }
+
 
   def map(list: List[Int], function: Int => Int) = {
     def _map(input: List[Int], running: List[Int]): List[Int] = {
@@ -24,13 +38,13 @@ object ListUtils {
   }
 
   def partition(ints: List[Int], function: Int => Boolean) = {
-      def _partition(input: List[Int], trueList: List[Int], falseList: List[Int]): List[List[Int]] = {
-        if(input isEmpty) List(trueList, falseList)
-        else if(function(input.head)) _partition(input.tail, trueList :+ input.head, falseList)
-        else  _partition(input.tail, trueList , falseList:+ input.head)
+    def _partition(input: List[Int], trueList: List[Int], falseList: List[Int]): List[List[Int]] = {
+      if (input isEmpty) List(trueList, falseList)
+      else if (function(input.head)) _partition(input.tail, trueList :+ input.head, falseList)
+      else _partition(input.tail, trueList, falseList :+ input.head)
 
-      }
-      _partition(ints, List[Int](), List[Int]())
+    }
+    _partition(ints, List[Int](), List[Int]())
   }
 
 }
--- src/test/scala/listutils/ListUtilsTest.scala
+++ src/test/scala/listutils/ListUtilsTest.scala
@@ -19,6 +19,18 @@ class ListUtilsTest extends FlatSpec with ShouldMatchers {
   "A Partition function" should ("partition a list into two using the result of a predicate passed in") in {
     ListUtils.partition(List(1,2,3,4), x => x %2 == 0) should be (List(List(2,4),List(1,3)))
   }
+  "A Nth Element of function" should ("locate and return the nth element of a list for a list of size 1") in {
+    ListUtils.nthElement(List(1),0) should be (1)
+  }
+  it should ("locate and return the nth element of a list for a list of size greater than 1") in {
+    ListUtils.nthElement(List(1,2,3),2) should be (3)
+  }
+  it should ("throw an IndexOutOfBoundsException when the supplied index is out of bounds of the input List") in {
+    an [IndexOutOfBoundsException] should be thrownBy  (ListUtils.nthElement(List(1,2,3), 3))
+  }
+  it should ("throw an IndexOutOfBoundsException when the supplied index is out of bounds of the input List for an empty list") in {
+    an [IndexOutOfBoundsException] should be thrownBy  (ListUtils.nthElement(List(), 0))
+  }
 
 
 }
